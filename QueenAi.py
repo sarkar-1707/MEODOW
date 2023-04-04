7f5035000a0322ec9695f372f3e73a9c6622bc63
Itzvenomo <118875769+Itzvenomo@users.noreply.github.com>
2023-04-04 11:15:30 +0530

    Add files via upload

--- QueenAi.py
+++ QueenAi.py
@@ -1,11 +1,8 @@
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 import asyncio
 from pyrogram.types import *
 from pymongo import MongoClient
 import requests
-from datetime import datetime
-
-
 import random
 from pyrogram.errors import (
     PeerIdInvalid,
@@ -289,60 +286,3 @@ async def queenprivatesticker(client: Client, message: Message):
                
 
 client.run()
-
-
-@Client.on_message(filters.command(["stats", "status"], ".") & filters.me)
-async def stats(client: Client, message: Message):
-    Man = await message.edit_text("`ᴄᴏʟʟᴇᴄᴛɪɴɢ sᴛᴀᴛs...`")
-    start = datetime.now()
-    u = 0
-    g = 0
-    sg = 0
-    c = 0
-    b = 0
-    a_chat = 0
-    Meh = await client.get_me()
-    async for dialog in client.get_dialogs():
-        if dialog.chat.type == enums.ChatType.PRIVATE:
-            u += 1
-        elif dialog.chat.type == enums.ChatType.BOT:
-            b += 1
-        elif dialog.chat.type == enums.ChatType.GROUP:
-            g += 1
-        elif dialog.chat.type == enums.ChatType.SUPERGROUP:
-            sg += 1
-            user_s = await dialog.chat.get_member(int(Meh.id))
-            if user_s.status in (
-                enums.ChatMemberStatus.OWNER,
-                enums.ChatMemberStatus.ADMINISTRATOR,
-            ):
-                a_chat += 1
-        elif dialog.chat.type == enums.ChatType.CHANNEL:
-            c += 1
-
-    end = datetime.now()
-    ms = (end - start).seconds
-    await Man.edit_text(
-        """`Your Stats Obtained in {} seconds`
-`You have {} Private Messages.`
-`You are in {} Groups.`
-`You are in {} Super Groups.`
-`You Are in {} Channels.`
-`You Are Admin in {} Chats.`
-`Bots = {}`""".format(
-            ms, u, g, sg, c, a_chat, b
-        )
-    )
-
-
-add_command_help(
-    "stats",
-    [
-        ["stats", "to check your account status."],
-    ],
-)
-
-
-
-
-
